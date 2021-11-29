5af01c0bb4f05f71a23f60317d3735b3f88c197d
Dungna89 <dungna29@fpt.edu.vn>
2021-11-29 18:19:50 +0700

    1

---
+++ 5_CP17311_COM108_BLOCK2_FALL21/Bai_1.9_HamTraVe/Main.c
@@ -0,0 +1,50 @@
+﻿#include <stdio.h>
+/*
+* Hàm trả về - trả về các giá trị
+*	1. Không tham số
+*	2. Có tham số
+* Cách sử dụng:
+*	1. Tên hàm phải có nghĩa
+*	2. Tên hàm luôn có động từ đứng trước:
+*		Ví dụ: tinhTong(),layDsSinhVien, getLstSinhVien.....
+*
+  Công thức:
+  <Kiểu dữ liệu trả về> <Tên hàm> (<Các tham số truyền vào>)
+  {
+		//Body code
+		return <Đúng Kiểu dữ liệu của hàm>;//Luôn nằm ở cuối cùng
+  }
+*/
+//Khai báo hàm không triển khai trước main
+int phepTru(int a, int b);
+int phepTru2();
+int main()
+{
+	//Cách gọi hàm trả về: Cần phải biết tên hàm trả về 1 giá trị có kiểu dữ liệu là gì.
+	phepTru(10, 5);//Không thấy in ra màn hình bởi cả 1 hàm là 1 giá trị muốn in ra thì phải dùng câu lệnh in ra màn hình
+	printf_s("%d", phepTru(20, 5)+2000);
+
+	printf_s("Hieu cua 2 so la: %d", phepTru2());
+}
+int phepTru(int a, int b)
+{
+	return a - b;//Là 2 số nguyên trừ cho nhau và phải trả ra 1 giá trị nguyên.
+}
+
+int phepTru2()
+{
+	int a, b;
+	printf("Nhap a: ");
+	scanf_s("%d", &a);
+	printf("Nhap b: ");
+	scanf_s("%d", &b);
+	return a - b;
+}
+char method1()
+{
+	return 'a';
+}
+double method2()
+{
+	return 8.6;
+}
---
+++ 5_CP17311_COM108_BLOCK2_FALL21/Bai_2.0_Mang_Array/Main.c
@@ -0,0 +1,88 @@
+﻿#include <stdio.h>
+/*
+ *  Mảng - ARRAY
+ *	1. Mảng dùng để lưu trữ 1 tập giá trị có cùng kiểu dữ liệu
+ *	2. Mảng trong lập trình là mảng tĩnh không co giãn
+ *	3. Để truy xuất và gán phần tử trong mảng cần biết index và index luôn bắt đầu từ 0
+ */
+int main()
+{
+	//Khai báo biến lưu trữ 1 giá trị
+	int a = 5;
+	a = 99;
+
+	//Phần 1: Cách dùng và khởi tạo
+	// <kiểu dữ liệu> <arr + tên mảng>[] = {giá trị 1,giá trị 2...}; 
+	// <kiểu dữ liệu> <arr + tên mảng>[kích thước mảng số nguyên];
+
+	int arrNumbers[] = { 99,12,33 };//Khởi tạo 1 mảng số nguyên có sẵn 3 giá trị ban đầu.
+	int arrYears[4];//Khởi tạo 1 mảng số nguyên có kích thước là 4 phần tử.
+	//Các bạn có thể tạo ra các mảng có các kiểu dữ liệu khác nhau
+
+	//Phần 2: Truy xuất và gán giá trị thông qua index
+	//arrYears = 2021; Lỗi vì không xác định được lưu vào đâu
+	arrYears[0] = 2021;//[0] = Index 0
+	arrYears[1] = 2022;
+	arrYears[2] = 2023;
+	arrYears[3] = 2024;
+	arrYears[0] = 1999;//Ghi đè lại giá trị
+
+	//Truy xuất giá trị trong mảng gán vào đâu thì lấy ra vậy
+	printf_s("Index 0 = %d \n", arrYears[0]);
+	printf_s("Index 1 = %d \n", arrYears[1]);
+	printf_s("Index 2 = %d \n", arrYears[2]);
+	printf_s("Index 3 = %d \n", arrYears[3]);
+
+	printf_s("======================\n");
+	//Cách để in tất cả các phần tử trong mảng ra màn hình dùng vòng lặp
+	//Index thì không âm
+	for (int i = 0; i < 4; ++i)
+	{
+		printf_s("Index 0 = %d \n", arrYears[i]);
+	}
+	/*
+	 * Bài 1: Khai báo 1 mảng số nguyên gồm các phần tử sau
+	 * int arrNumberFpoly[] = {88,7,9,6,5,8};	 *
+	 *  - In số 5 ra màn hình bằng câu printf.
+	 *  - In tất cả các phần tử có ở trong mảng ra màn hình
+	 *  - In tất cả các giá trị chẵn ra màn hình.
+	 *
+	 *	Bài 2: Cho người dùng nhập vào 1 mảng số nguyên và in tổng dãy số người dùng đã nhập ra màn hình.
+	 *
+	 *	18h Chữa 1 vài bài
+	 *
+	 */
+	//Bài 1: 
+	int arrNumberFpoly[] = { 88,7,9,6,5,8 };
+	printf_s("%d \n", arrNumberFpoly[4]);
+	for (int i = 0; i < 6; ++i)
+	{
+		printf_s("%d ", arrNumberFpoly[i]);
+	}
+	printf_s("\n");
+	for (int i = 0; i < 6; ++i)
+	{
+		if (arrNumberFpoly[i]%2==0)
+		{
+			printf_s("%d ", arrNumberFpoly[i]);
+		}
+	}
+	printf_s("\n");
+	//Bài 2:
+	int arrNumberBai2[200];
+	int soLuong,tong;
+	printf_s("Ban muon bao nhieu: ");
+	scanf_s("%d", &soLuong);
+	for (int i = 0; i < soLuong; ++i)
+	{
+		printf_s("Moi ban nhap vao lan thu %d: ", i);
+		scanf_s("%d", &arrNumberBai2[i]);
+	}
+	for (int i = 0; i < soLuong; ++i)
+	{
+		printf_s("%d ", arrNumberBai2[i]);
+		tong += arrNumberBai2[i];
+	}
+	printf_s("\n");
+	printf_s("Tong = %d", tong);
+}
